541ac5d49dcbdfa0ad136cc4933b39b93a1f9403
Cliff Syner <c4syner@gmail.com>
2022-09-13 18:05:59 -0500

    Fix dep hell

--- templates/greeter_example/hardhat.config.ts
+++ templates/greeter_example/hardhat.config.ts
@@ -3,7 +3,7 @@ require("@matterlabs/hardhat-zksync-solc");
 
 module.exports = {
   zksolc: {
-    version: "1.1.5",
+    version: "1.1.0",
     compilerSource: "docker",
     settings: {
       optimizer: {
@@ -11,14 +11,14 @@ module.exports = {
       },
       experimental: {
         dockerImage: "matterlabs/zksolc",
-        tag: "v1.1.5"
+        tag: "v1.1.0"
       }
     },
   },
   zkSyncDeploy: {
-    zkSyncNetwork: "$ATLAS_HTTP_RPC",
+    zkSyncNetwork: "http://ec2-184-72-69-110.compute-1.amazonaws.com:3050",
     ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-    account: "$PRIVATE_KEY"
+    account: "0xa86daed2bb5341cba34fd9c786bf09ec57e9e6b8d4f1428704ed428020d582e3"
   },
   networks: {
     hardhat: {
